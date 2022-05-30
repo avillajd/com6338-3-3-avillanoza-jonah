6c7e4b0ff748d971ea9cea67a2f2dd1e64a31347
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-05-29 21:49:43 -0400

    Assignment 3 Attempt

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
     var questionsArr = [
         {
             question: 'JavaScript is the best language',
@@ -32,18 +31,53 @@
         alert("You have answered " + score + "% correct.");
     }
 
-    // Troubleshooting Having a hard time with this one
-    // function runQuiz() {
-    //     var choice = 0 // from the example... 
-    //     for(var i = 0, i < questionsArr.length; i ++) {
-    //         if(confirm(questionsArr[i].question)=== questionsArr[i].answer){
-    //             choice++
-    //         };
 
-    //     }
-    //     var score = Math.round((choice/questionsArr.length)*100);
-    //     alert("You have answered" + score + "%")
 
-    //}
+
+    // // Troubleshooting Having a hard time with this one
+    // // function runQuiz() {
+    // //     var choice = 0 // from the example... 
+    // //     for(var i = 0, i < questionsArr.length; i ++) {
+    // //         if(confirm(questionsArr[i].question)=== questionsArr[i].answer){
+    // //             choice++
+    // //         };
+
+    // //     }
+    // //     var score = Math.round((choice/questionsArr.length)*100);
+    // //     alert("You have answered" + score + "%")
+
+    // //}
     
-   
+//    var questionsArr = [
+//     {
+//         question: 'is the moon a planet?',
+//         answer: false
+//     }, 
+//     {
+//         question: 'the year is 2022.',
+//         answer: true
+//     },
+//     {
+//         question: 'dogs sweat out of their skin.',
+//         answer: false
+//     },
+//     {
+//         question: 'red is a primary color.',
+//         answer: true
+//     },
+//     {
+//         question: 'Pokemon was a cartoon orginating in the UK.',
+//         answer: false
+//     },
+// ];
+// function runQuiz() {
+//     var numOfAgrees = 0;
+//     for (var i = 0; 0 < questionsArr.length; i++){
+//         if(confirm(questionsArr[i].question) == questionsArr.answer){
+//             numOfAgrees++;
+//         };
+//     }
+        
+//         var score = Math.round(numOfAgrees/questionsArr.question)*100;
+//         alert ('You scored' + score + '% of the questions correct!' )
+//     }
